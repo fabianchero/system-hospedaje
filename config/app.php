7e6721461775f1507f76afc5733795686b7949e1
Fabian Chero Ordinola <chero122004@gmail.com>
2024-08-19 18:56:56 -0500

    Continuar al video 11

---
+++ config/app.php
@@ -0,0 +1,7 @@
+<?php
+
+    const APP_URL = "http://localhost/system-hospedaje/";
+    const APP_NAME = "Hospedaje";
+    const APP_SESSION_NAME = "system-hospedaje";
+
+    date_default_timezone_set("America/Lima");
---
+++ config/server.php
@@ -0,0 +1,5 @@
+<?php
+    const DB_SERVER= "localhost";
+    const DB_NAME= "bd_hospedaje";
+    const DB_USER= "root";
+    const DB_PASS= "";
